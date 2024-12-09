53b7834a018058eea861804d764577b001b40d3d
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-12-09 13:09:58 +0200

    Added Main Menu

---
+++ Hafaka 3/Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
